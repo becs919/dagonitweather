3c9b3c52698b3eee9be430354a7e479552d7d20c
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-08-18 09:59:53 -0600

    Add all specs to history cards as well as styling

--- src/WeatherHistory/WeatherHistory.js
+++ src/WeatherHistory/WeatherHistory.js
@@ -3,9 +3,17 @@ import './WeatherHistory.css';
 
 const WeatherHistory = ({ history }) => {
   return (
-    <div className="history">
-      <h3 tabIndex="0">Past Cities</h3>
-      { history.map(city => <p tabIndex="0" key={ city }>{ city }</p>) }
+    <div className="history-div">
+      <h3 className="past-cities" tabIndex="0">Search History</h3>
+      <div className="history">
+        { history.map(city => <div key={city.time} className="history-card">
+        <p tabIndex="0" className="current">{city.current}&deg;F</p>
+        <img src={`http://openweathermap.org/img/w/${city.icon}.png`} alt="weather icon"/>
+        <p tabIndex="0" className="name">{city.name}</p>
+        <p tabIndex="0" className="description">{city.description}</p>
+        <p tabIndex="0" className="time">at {city.time}</p>
+        </div>) }
+      </div>
     </div>
   )
 }
